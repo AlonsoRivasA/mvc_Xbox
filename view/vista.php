603375559a026fbecf598e02d2d6773e90518fa4
RivasA128 <143743275+AlonsoRivasA@users.noreply.github.com>
2023-11-06 13:38:46 -0700

    Add files via upload

---
+++ view/vista.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8" />
+	<title>Modelo-vista-controlador</title>
+</head>
+<body>
+	<h1>Juegos disponibles</h1>
+	<table border="1">
+		<tr>
+			<td><strong>ID</strong></td>
+			<td><strong>Nombre Juego</strong></td>
+			<td><strong>Descripcion Juego</strong></td>
+			<td><strong>Genero Juego</strong></td>
+			<td><strong>Fecha Juego</strong></td>
+			<td><strong>Precio Juego</strong></td>
+			<td><strong>Clasificacion Juego</strong></td>
+			<td><strong>Desarrollador Juego</strong></td>			
+		</tr>
+		<?php
+			for($i=0;$i<count($pd);$i++)
+			{
+				?>
+					<tr>
+						<td><?php echo $pd[$i]["id"]; ?></td>
+						<td><?php echo $pd[$i]["nombre"]; ?></td>
+						<td><?php echo $pd[$i]["descripcion"]; ?></td>
+						<td><?php echo $pd[$i]["genero"]; ?></td>
+						<td><?php echo $pd[$i]["fecha"]; ?></td>
+						<td><?php echo $pd[$i]["precio"]; ?></td>
+						<td><?php echo $pd[$i]["clasificacion"]; ?></td>
+						<td><?php echo $pd[$i]["desarrolladores"]; ?></td>
+					</tr>
+				<?php
+			}
+		?>
+	</table>
+</body>
+</html>
